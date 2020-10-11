78d7621b85e9516472c8847283581375b3a01b66
Jeffrey Harding <jeffreyh202@gmail.com>
2020-10-11 15:11:02 -0600

    route updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-let db = require(__dirname, "Develop/db/db,json");
+let db = require(__dirname, "db/db,json");
 
 
 var app = express();
@@ -10,50 +10,52 @@ var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("Develop/public"));
+app.use(express.static("./public"));
 
 // view routes
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 //api routes
-app.get("/api/notes", function(req, res) {
-    fs.readFile(__dirname, "Develop/db/db.json", (err, data) => {
-        let noteList = JSON.parse(data)
-        res.json(noteList)
-    })
-});
 
-app.post("/api/notes", function(req, res) {
-    fs.readFile(__dirname, "Develop/db/db.json", (err, data) => {
-        if (err)
-        throw (err);
-        let noteList = JSON.parse(data);
-        let addNote = req.body
-        addNote.id = Math.floor(Date.now() * Math.random())
-        noteList.push(addNote)
-
-        fs.writeFile(__dirname, "Develop/db/db.json", JSON.stringify(addNote), (err, data) => {
-            if (err)
-            throw (err)
-            res.json(addNote)
-        })
+    app.get("/api/notes", function(req, res) {
+        // let notes = fs.readFileSync("./db/db.json");
+        // let list = JSON.parse(notes);
+        // console.log("hello?")
+        res.json(db);
     });
     
-})
+    app.post("/api/notes", function(req, res) {
+            let addNote = req.body;
+            addNote.id = Math.floor(Date.now() * Math.random());
+            let noteList = fs.readFileSync("./db/db.json");
+            let parsedList = JSON.parse(noteList);
+            parsedList.push(addNote);
+    
+            fs.writeFileSync("./db/db.json", JSON.stringify(parsedList), (err) => {
+                if (err)
+                throw (err)
+                res.json(parsedList)
+            })
+        });
+        
+    
+    
+    app.delete("/api/notes/:id", function(req, res) {
+        res.json(db)
+    })
+    
 
-app.delete("/api/notes/:id", function(req, res) {
-    res.json(db)
-})
+
 
 app.listen(PORT, () => {
     console.log("Application is listening on PORT " + PORT);
